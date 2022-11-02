f0c9b6cc55e0033f01f9dab8f5f31f39a2cfd136
alikha25 <mahmoudbechki@gmail.com>
2022-11-02 22:07:25 +0100

    reading secret key securly

--- marko/marko/settings.py
+++ marko/marko/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-s)p+k03u+g+5td2mr0d6p5=cq9*m%jn@phzcv^87+69no2435x'
+f = open("secret.txt",'r')
+SECRET_KEY = f.read()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
