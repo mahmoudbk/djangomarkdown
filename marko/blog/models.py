59cd8d2f0226fccfbe3f2d1c8218a9ffd01dd44f
alikha25 <mahmoudbechki@gmail.com>
2022-11-03 11:18:49 +0100

    added markdown

--- marko/blog/models.py
+++ marko/blog/models.py
@@ -4,3 +4,6 @@ class Post(models.Model):
     title = models.CharField(max_length=250,null=True,blank=True)
     body = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.title)
--- marko/blog/views.py
+++ marko/blog/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
-# Create your views here.
+from .models import Post
+def blog_detail(request,id):
+    blog = Post.objects.get(pk=int(id))
+    context = {'post':blog}
+    return render(request,'blog_detail.html',context)
